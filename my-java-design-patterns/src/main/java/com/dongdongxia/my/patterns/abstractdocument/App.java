3a2314201178712c0b7b5845d8d79f9a196ae834
liudong <java_liudong@163.com>
2017-08-24 10:11:29 +0800

    添加： 两个实体对象，用于填充目录

---
+++ my-java-design-patterns/src/main/java/com/dongdongxia/my/patterns/abstractdocument/App.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.my.patterns.abstractdocument;
+/**
+ * 
+ * <P>Description: 抽象文档测试， 属于结构型类型</P>
+ * @ClassName: App
+ * @author java_liudong@163.com  2017年8月24日 上午10:07:32
+ */
+public class App {
+
+}
---
+++ my-java-design-patterns/src/main/java/com/dongdongxia/my/patterns/abstractdocument/domain/Car.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.my.patterns.abstractdocument.domain;
+/**
+ * 
+ * <P>Description: 汽车实体</P>
+ * @ClassName: Car
+ * @author java_liudong@163.com  2017年8月24日 上午10:10:56
+ */
+public class Car {
+
+}
