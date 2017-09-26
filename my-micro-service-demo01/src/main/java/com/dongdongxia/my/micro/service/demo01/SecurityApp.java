660210923cfcfc0e974060a3e632f249e9a479b3
liudong <java_liudong@163.com>
2017-09-26 09:28:37 +0800

    添加： 测试的对象

---
+++ my-micro-service-demo01/src/main/java/com/dongdongxia/my/micro/service/demo01/SecurityApp.java
@@ -0,0 +1,55 @@
+package com.dongdongxia.my.micro.service.demo01;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * <P>Description: Spring Security 对象的使用</P>
+ * @ClassName: SecurityApp
+ * @author java_liudong@163.com  2017年9月21日 下午3:55:58
+ */
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class SecurityApp {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApp.class, args);
+    }
+    
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+        
+        @Autowired
+        private AuthenticationManager authenticationManager;
+        
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+        
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+            .withClient("acme")
+            .secret("acmesecret")
+            .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+            .scopes("webshop");
+        }
+    }
+}
